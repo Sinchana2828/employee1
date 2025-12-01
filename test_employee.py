d701904c6eb437896c1d77f97c0fa744f91a065d
Sinchana2828 <sinchanaballary@gmail.com>
2025-12-01 17:06:10 +0530

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -1,5 +1,4 @@
 from employee import employee_details
-
 def test_employee_details():
     expected_output = (
         "Employee Name: Alice\n"
@@ -7,5 +6,4 @@ def test_employee_details():
         "Employee department: IT\n"
         "Salary : 55000\n"
     )
-
     assert employee_details("Alice", "E1001", "IT", 55000) == expected_output
